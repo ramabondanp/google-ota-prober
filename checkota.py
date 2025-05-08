741f2d4e70267893f6684124afd29ad9e1ccdb27
Rama Bondan Prakoso <ramanarubp@gmail.com>
2025-05-08 13:17:42 +0700

    checkota: Change killall to pkill
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- checkota.py
+++ checkota.py
@@ -343,7 +343,7 @@ def get_fingerprint(url: str, proxy: Optional[str] = None) -> Optional[str]:
     cmd = (
         f"curl --fail -Ls{proxy_opt} --max-time 60 --limit-rate 100K {shlex.quote(url)} "
         f"| ( bsdtar -Oxf - 'META-INF/com/android/metadata' 2>/dev/null || true ) "
-        f"| ( grep -m1 '^post-build=' | sed 's/^post-build=//' && killall curl ) "
+        f"| ( grep -m1 '^post-build=' | sed 's/^post-build=//' && pkill curl ) "
         f"2>/dev/null"
     )
 
